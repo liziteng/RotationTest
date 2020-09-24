29247a131d6ebe81bfe4f5cacc4478a1b3383938
liziteng <liziteng@qq.com>
2020-09-24 13:39:24 +0800

    转盘的测试，使用A、D键来控制左转和右转。没有按键时圆盘自动归位。 当前问题：圆盘当旋转角度大于180°的时候，圆盘会就近归位而不会计算多转的180° 目标：在圆盘归位时将之前所转的所有角度都转回去。

--- Assets/Test/PanelRotation.cs
+++ Assets/Test/PanelRotation.cs
@@ -1,21 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 public class PanelRotation : MonoBehaviour
 {
+    //这个脚本在"转盘"物体上.
+    private Quaternion originalAngle;
+
     public bool isTurning;
 
+    void Start()
+    {
+        print("Press A or D to rotate panel.");
+        originalAngle = transform.localRotation;
+    }
     void Update()
     {
-
+        RotationControl(KeyCode.A, -6f);
+        RotationControl(KeyCode.D, 6f);
+   
+        if (!isTurning)
+        {
+            RotationReset();
+        }
     }
-    private void RotationReset()
+    private void RotationReset() //这个是角度重置. 当前角度转回到最开始的角度
     {
-
+        transform.localRotation =
+        Quaternion.RotateTowards(transform.localRotation, originalAngle, 100 * Time.deltaTime);
     }
 
-    private void RotationControl()
+    private void RotationControl(KeyCode key, float speed) //这个是A、D键控制旋转
     {
-
+        if (Input.GetKey(key))
+        {
+            isTurning = true;
+            transform.eulerAngles += new Vector3(0, 0, speed);
+        }
+        else if (Input.GetKeyUp(key))
+        {
+            isTurning = false;
+        }
     }
 }
